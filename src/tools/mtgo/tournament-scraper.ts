58215f63e65da962d79b3b8ab71f49edb89a428b
eiwos <mahmoudi.otman@gmail.com>
2024-01-01 12:03:45 +0100

    Modification according to mtgo

--- src/tools/mtgo/tournament-scraper.ts
+++ src/tools/mtgo/tournament-scraper.ts
@@ -7,15 +7,17 @@ import { superFetch } from '../../core/utils';
 /**
  *
  */
-export const MTGOTournamentScraper = async (month: number, year: number) => {
+export const MTGOTournamentScraper = async (month: number, year: number, isWWW = true) => {
+  const base = isWWW ? 'www.mtgo' : 'mtgo';
   const m = (month +1).toString();
   const goodMonth = m.length === 1 ? `0${m}`: m;
 
-  const url = `https://www.mtgo.com/en/mtgo/decklists/${year}/${goodMonth}`;
+
+  const url = `https://${base}.com/decklists/${year}/${goodMonth}`;
   const data = await superFetch(url);
 
   const document = new JSDOM(data).window.document;
   const listOfElements = Array.from(document.querySelectorAll('.decklists-item'));
 
-  return listOfElements.map(element => 'https://www.mtgo.com'+element.firstElementChild?.getAttribute('href'));
+  return listOfElements.map(element => `https://${base}.com${element.firstElementChild?.getAttribute('href')}`);
 };
